1439c1d53e87bf10eab3fda361036acf595c0c2d
5cloud <34443647+5cloud@users.noreply.github.com>
2017-12-29 17:56:01 +0800

    Update test-pages.js
    v1

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,7 +3,7 @@ var request = require('request');
 var server = process.env['SERVER']||localhost;
 it('Main page content', function(done) {
     request('http://' + server + ':3000' , function(error, response, body) {
-      expect(body).to.equal('Hello World');
+      expect(body).to.equal('Hello World v1');
       done();
     });
 });
